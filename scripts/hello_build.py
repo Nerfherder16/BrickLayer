c779c00f52c6cec9496934d69c8ed21a080a186d
Tim <trg16@users.noreply.github.com>
2026-04-06 22:44:34 -0400

    chore: update .autopilot, scripts, tests (3 files)

--- scripts/hello_build.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--name", required=True)
-args = parser.parse_args()
-print(f"Hello, {args.name}! Build system works.")
